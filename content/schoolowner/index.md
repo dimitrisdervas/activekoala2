a30f69f7ae873ff10928cf96cb103e47197edfee
Dimitris <dimitris@techangel.gr>
2018-08-15 23:48:21 +0300

    started gulpfile to create two taxonomies subcategory and city

---
+++ content/schoolowner/index.md
@@ -0,0 +1,45 @@
++++
+date = "2016-11-05T21:05:33+05:30"
+title = "Καταχώρηση"
+index= "false"
++++
+
+Η καταχώρηση θα είναι πάντα δωρεάν και οι χρεώσεις θα αφορούν για τον έξτρα κόπο να προσθέτουμε τρόπους επικοινωνίας όπως messenger chat, online forms, κτλ.
+
+
+### Βήμα 1
+
+
+**Απλή καταχώρηση:** Απλά στείλτε τη διεύθυνση της σελίδας σας στο Facebook και εμείς θα σας καταχωρήσουμε στο ActiveKoala.
+
+<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" name="contact" method="POST" netlify-honeypot="bot-field" netlify>
+	<p class="hidden">
+    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+  </p>
+  <div>
+    <label class="block text-grey-darker text-x1 font-bold mb-2">Oνομα:</label>
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"type="text" name="name" />
+  </div> 
+   <div>
+    <label>Email:</label> <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" name="email" />
+  </div>
+     <div>
+    <label>Facebook Page: <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" name="facebook" /></label>
+  </div>
+  <div>
+    <label>ΣΧΟΛΙΑ: <textarea name="message"></textarea></label>
+  </div>
+
+  <div data-netlify-recaptcha></div>
+  <div>
+ <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type=”submit”>Αποστολή</button>
+  </div>
+
+</form>
+
+
+### Βήμα 2
+
+**Απλή καταχώρηση:** Στη συνέχεια θα σας στείλουμε το λινκ της καταχώρησης με τα στοιχεία που βρήκαμε στο facebook.
+
+<script src="https://widget.flowxo.com/embed.js" data-fxo-widget="eyJ0aGVtZSI6IiMwMDkyY2MiLCJ3ZWIiOnsiYm90SWQiOiI1YjZjMDA3ZTM5YzM2ODAwNjI4NWZlZWUiLCJ0aGVtZSI6IiMwMDkyY2MifX0=" async defer></script>
---
+++ gulp/tasks/md-subcategory-city.js
@@ -0,0 +1,79 @@
+var gulp            = require('gulp');
+var Papa            = require("papaparse");
+var fs              = require('fs');
+var nunjucks     = require('gulp-nunjucks');
+var nunjucksRender  = require('gulp-nunjucks-render');
+var translit        = require('speakingurl');
+var rename          = require('gulp-rename');
+var shell           = require('shelljs');
+var runSequence     = require('run-sequence');
+var del             = require('del');
+var gulpSequence = require('gulp-sequence')
+
+
+var subcategoriesPath = {};
+subcategoriesPath.collection = 'content/subcategoriesCity';
+subcategoriesPath.csv        = 'csv/google/subcategories';
+subcategoriesPath.template   = '_gulp-templates/nunjucks/subcategoriesCity.html';
+subcategoriesPath.templateyml   = '_gulp-templates/nunjucks/subcategoriesyml.html';
+module.exports = subcategoriesPath;
+
+
+gulp.task('md:subcategoriesCity', function() {
+
+    fs.readFile('./'+ subcategoriesPath.csv +'.csv', 'utf8', function(err, data){
+        if (err) throw err;
+
+        parsed = Papa.parse(data,{delimiter: ',',   newline: ''});
+        rows = parsed.data;
+
+        for(var i = 1; i < (rows.length)-1; i++) {
+            var items = rows[i]
+
+         // https://gist.github.com/antonreshetov/c41a13cfb878a3101196c3a62de81778
+            var subcategoryTranslit = translit(items[1], {
+                lang: 'en'
+              })
+
+            var templateData = {
+                subcategory : items[1],
+                category: items[2],
+                SchoolsUID: items[3],
+                slugSubcategory:  items[4],
+                slugCategory: items[5],
+                schoolscount: items[6],
+                subcategoryTranslit: subcategoryTranslit,
+            };
+
+
+
+            gulp.src(subcategoriesPath.template)
+                .pipe(nunjucksRender({
+                  data: templateData
+                }))
+                .pipe(rename({
+                    dirname: subcategoriesPath.collection + "/" + templateData.slugSubcategory,
+                    basename: "_index",
+                    extname: ".md"}))
+                .pipe(gulp.dest('.'));
+            }
+      });
+
+});
+
+gulp.task('del:subcategoriesCity', function () {
+  return del([
+    // here we use a globbing pattern to match everything inside the `mobile` folder
+    'content/subcategoriesCity/**/*'
+  ]);
+});
+
+gulp.task('create:subcategoriesCity', gulpSequence(
+    'del:subcategoriesCity',
+    'md:subcategoriesCity',
+    ));
+
+gulp.task('recreate:subcategoriesCity', gulpSequence(
+    'del:subcategoriesCity',
+    'md:subcategoriesCity',
+    ));
