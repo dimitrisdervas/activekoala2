2b556b9d11427faebaa277227abde61328a8f28d
Dimitris <dimitris@techangel.gr>
2018-11-23 19:44:29 +0200

    added md subcategory gulp tsk to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,8 @@ gulp.task('del:all', function () {
     'content/categories/**/*',
     'data/yml/categories/**/*',
     'content/subcategories/**/*',
+    'content/subcategoryathens/**/*',
+    'content/subcategorythessaloniki/**/*',
     'data/yml/subcategories/**/*',
     'content/categoriesathens/**/*',
     'data/yml/categoriesathens/**/*',
@@ -65,7 +67,7 @@ gulp.task('del:all', function () {
 });
 
 
-gulp.task('default:create', gulpSequence( 'del:all','download:csvs',['md:schools','md:categories','md:subcategories','md:athenscategories','md:thesscategories'],['yml:subcategories','yml:categories','yml:athenscategories','yml:thesscategories']))
+gulp.task('default:create', gulpSequence( 'del:all','download:csvs',['md:schools','md:categories','md:subcategories','md:athenscategories','md:thesscategories','md:subcategoryathens','md:subcategorythessaloniki'],['yml:subcategories','yml:categories','yml:athenscategories','yml:thesscategories']))
 
 
 gulp.task('default:createmd', gulpSequence( 'del:all',['md:schools','md:categories','md:subcategories','md:athenscategories','md:thesscategories'],['yml:subcategories','yml:categories','yml:athenscategories','yml:thesscategories']))
